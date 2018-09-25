8ab3ca8915c7db5bd05cb99f9ab4872dfdcf25b2
d86leader <d86leader@mail.com>
2018-09-25 17:11:19 +0700

    added code to read sector, chaged printing

--- loader.asm
+++ loader.asm
@@ -12,14 +12,17 @@ mov gs, ax
 mov ss, ax
 jmp 0x0:start
 
-;; DATA SECTION
+;; ----- DATA SECTION ----- ;;
 
 message:
-  db "hello BIOS world", 0
+  db "trying to read sector...", 0
 
-;; START
+;; ----- START ----- ;;
 
 start:
+
+;; ----- Fill screen with spaces ----- ;;
+
 ;; select active page
 xor al, al ;; page 0
 mov ah, 0x5
@@ -32,25 +35,8 @@ xor dl, dl ;; col 0
 mov ah, 0x2
 int 0x10
 
-
-;; load pointer to source text
-mov si, message
-;; reset symbol counter
+;; char printing loop
 xor cx, cx
-
-print_char:
-  ;; load char, quit if zero
-  lodsb
-  test al, al
-  jz spaces
-
-  mov ah, 0xe ;; print char
-  int 0x10
-
-  inc cx
-  jmp print_char
-
-
 spaces:
   mov al, 0x20
   mov ah, 0xe
@@ -59,9 +45,64 @@ spaces:
   cmp cx, 80*25 - 1
   jb spaces
 
+;; ----- Put greeting ----- ;;
+mov si, message
+call putstr
+
+jmp loop_mark
+
+;; ----- Read next sector ----- ;;
+
+;; read first
+;; from:
+xor cx, cx ;; cylinder 0, sector 0
+inc cl
+inc cl     ;; sector 2. Starting sector is probably one
+xor dh, dh ;; head 0
+mov dl, 0x80 ;; first disk drive
+;; to:
+;; es already set to 0
+mov bx, 0x7c00 + 512 ;; just after this code
+;; amount to load
+mov al, 1
+;; perform
+mov ah, 2
+int 0x13
+
+;; print the string in loaded segment
+
+mov si, 0x7c00 + 512
+call putstr
+
 loop_mark:
 jmp loop_mark
 
 exit:
 xor al, al
 out 0xf4, al
+
+
+;; ARGS
+;;    ds:si - string to put, 0-terminated
+;;  NO regs preserved
+last_cursor_pos: db -1
+putstr:
+;; set cursor to position:
+  mov dh, [last_cursor_pos]
+  inc dh
+  mov [last_cursor_pos], dh
+  xor bh, bh ;; page 0
+  xor dl, dl ;; col 0
+  mov ah, 0x2
+  int 0x10
+
+ .putchar
+  lodsb
+  test al, al
+  jnz .exit
+  mov ah, 0xe
+  int 0x10
+  jmp .putchar
+
+ .exit: ret
+
